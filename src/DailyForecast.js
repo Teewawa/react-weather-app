19adff6599558ce6499327a84ff549be9d0225d5
Tiara Mendiola <dev.tiaram@gmail.com>
2022-09-20 12:55:18 -0500

    Created Daily Forecast component
    Daily Forecast Component will receive weather response from App.js and further display the DAILY/CURRENT forecast for city input

---
+++ src/DailyForecast.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function DailyForecast(props){
+    return(
+        <div className="DailyForecast">
+            <h1>daily forecast</h1>
+        </div>
+    )
+}
