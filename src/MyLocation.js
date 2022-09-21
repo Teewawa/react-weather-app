9cd25beced2a65f827af0a61d8d1e33b7160f110
Tiara Mendiola <dev.tiaram@gmail.com>
2022-09-21 15:46:50 -0500

    Created MyLocation component
    This snip includes the button, utilizing some bootstrap class styling, that users may click to check the weather forecast for their current location. **Consider media queries for this button**

---
+++ src/MyLocation.js
@@ -0,0 +1,20 @@
+import React from "react";
+import './App.css';
+import myLocationIcon from "./images/myLocationIcon.png"
+
+export default function MyLocation(props){
+    return(
+        <div className="MyLocation">
+            <div className="LocationBttn">
+            <button
+              type="button"
+              className="btn btn-warning p-1 m-0"
+              id="currentLocationBttn"
+            >
+              Check my location{" "}
+              <img src={myLocationIcon} className="myLocationIcon" alt="" />
+            </button>
+          </div>
+        </div>
+    )
+}
